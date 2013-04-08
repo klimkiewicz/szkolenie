5dce7edb9fb69b256e6cf81852407290fbbba2b7
pk <pk@pk.(none)>
2013-04-08 10:41:51 +0200

    code

--- passwords.py
+++ passwords.py
@@ -1,6 +1,16 @@
+import hashlib
+
+salt = '123'
+
+
 def get_password_for_database(password):
-    return password
+    hashed = hashlib.sha1(password)
+    for i in range(3):
+        hashed = hashlib.sha1(salt + hashed.hexdigest())
+    return hashed.hexdigest()
 
 
 def check_password(user_provided, from_database):
-    return user_provided == from_database
+    return get_password_for_database(user_provided) == from_database
+
+
