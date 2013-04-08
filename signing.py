8e4ad58cf3bc9e116def10c464dc88b66d125a9a
Tomasz Czekanski <tomasz.czekanski@coders-mill.com>
2013-04-08 12:04:12 +0200

    signing

--- signing.py
+++ signing.py
@@ -1,6 +1,19 @@
+import hashlib
+SECRET = "asdadas ewr34552342 52w t54t 2"
+
 def sign_args(args):
+    h = hashlib.new('sha256')
+    argstr = str(args.to_dict())
+    for i in range(500):
+        hsh = h.update(argstr+SECRET)
+    args['chksum'] = h.hexdigest()
     return args
 
 
 def check_signature(args):
-    return True
+    chk = args.pop('chksum')
+    argsout = sign_args(args)
+    if chk == argsout['chksum']:
+        return True
+    else:
+        return False
