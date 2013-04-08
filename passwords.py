d06e1e43261f897c6bcf1135e461845cf863f526
Tomasz Czekanski <tomasz.czekanski@coders-mill.com>
2013-04-08 10:42:12 +0200

    first

--- passwords.py
+++ passwords.py
@@ -1,6 +1,15 @@
+import hashlib
+SECRET_KEY = 'asdassada89yr87qy452oh3874434939804298KROIW9J8'
+
 def get_password_for_database(password):
+    password = SECRET_KEY+password
+    h = hashlib.new('sha224')
+    for i in range(500):
+        password = h.update(password)
+        password = h.hexdigest()
     return password
 
 
 def check_password(user_provided, from_database):
+    user_provided = get_password_for_database(user_provided)
     return user_provided == from_database
